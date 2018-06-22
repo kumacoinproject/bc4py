542b73fe943aa6d2b3b116733bdd154055314a53
namuyan <thhjuu@yahoo.co.jp>
2018-06-22 21:32:56 +0900

    fix sending bug

--- bc4py/chain/checking/utils.py
+++ bc4py/chain/checking/utils.py
@@ -27,7 +27,7 @@ def inputs_origin_check(tx, include_block):
             pass  # OK
         # 使用済みかチェック
         # TODO: 正しく機能するか？
-        if txindex in tx_builder.get_usedindex(txhash):
+        if txindex in tx_builder.get_usedindex(txhash, include_block):
             raise BlockChainError('Input is already used! {}:{}'.format(hexlify(txhash).decode(), txindex))
 
 
