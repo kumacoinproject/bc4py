8bde0048a43c54eab97fb7d08935bd10c4d7be34
namuyan <thhjuu@yahoo.co.jp>
2018-10-01 21:44:52 +0900

    add observe-node

---
+++ observenode.py
@@ -0,0 +1,74 @@
+#!/user/env python3
+# -*- coding: utf-8 -*-
+
+from bc4py.config import Debug
+from bc4py.utils import set_database_path, set_blockchain_params
+from bc4py.user.boot import *
+from bc4py.user.network import broadcast_check, DirectCmd, sync_chain_loop
+import bc4py.user.network.synchronize as sync
+from bc4py.user.api import create_rest_server
+from bc4py.database.create import make_account_db
+from bc4py.database.builder import builder
+from bc4py.chain.workhash import start_work_hash, close_work_hash
+from p2p_python.utils import setup_p2p_params
+from p2p_python.client import PeerClient
+from p2p_python.config import C
+from bc4py.for_debug import set_logger
+import logging
+
+
+def work(port, sub_dir=None):
+    # BlockChain setup
+    set_database_path(sub_dir=sub_dir)
+    builder.set_database_path()
+    make_account_db()
+    start_work_hash()
+    genesis_block, network_ver, connections = load_boot_file()
+    logging.info("Start p2p network-ver{} .".format(network_ver))
+
+    # P2P network setup
+    setup_p2p_params(network_ver=network_ver, p2p_port=port, sub_dir=sub_dir)
+    pc = PeerClient()
+    pc.event.addevent(cmd=DirectCmd.BEST_INFO, f=DirectCmd.best_info)
+    pc.event.addevent(cmd=DirectCmd.BLOCK_BY_HEIGHT, f=DirectCmd.block_by_height)
+    pc.event.addevent(cmd=DirectCmd.BLOCK_BY_HASH, f=DirectCmd.block_by_hash)
+    pc.event.addevent(cmd=DirectCmd.TX_BY_HASH, f=DirectCmd.tx_by_hash)
+    pc.event.addevent(cmd=DirectCmd.UNCONFIRMED_TX, f=DirectCmd.unconfirmed_tx)
+    pc.event.addevent(cmd=DirectCmd.BIG_BLOCKS, f=DirectCmd.big_blocks)
+    C.MAX_RECEIVE_SIZE = 2000 * 1000  # 2Mb
+    pc.start()
+    V.PC_OBJ = pc
+
+    if pc.p2p.create_connection('tipnem.tk', 2000):
+        logging.info("Connect!")
+
+    for host, port in connections:
+        pc.p2p.create_connection(host, port)
+    set_blockchain_params(genesis_block)
+
+    # BroadcastProcess setup
+    pc.broadcast_check = broadcast_check
+
+    # Update to newest blockchain
+    builder.init(genesis_block)
+    sync_chain_loop()
+
+    # Mining/Staking setup (nothing)
+    Debug.F_WS_FULL_ERROR_MSG = True
+    # Debug.F_STICKY_TX_REJECTION = False  # for debug
+    logging.info("Finished all initialize. (no mining and staking)")
+
+    try:
+        create_rest_server(f_local=True, port=port+1000, user='user', pwd='password')
+        builder.db.batch_create()
+        builder.close()
+        pc.close()
+        close_work_hash()
+        sync.f_working = False
+    except KeyboardInterrupt:
+        logging.debug("KeyboardInterrupt.")
+
+
+if __name__ == '__main__':
+    set_logger(level=logging.DEBUG)
+    work(port=2000)
