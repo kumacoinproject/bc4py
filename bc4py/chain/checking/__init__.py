e90238147879878681941e4714d255bc8f03c779
namuyan <thhjuu@yahoo.co.jp>
2018-10-02 20:11:55 +0900

    bug fix: Reject new block on new_insert_block fnc

--- bc4py/chain/checking/__init__.py
+++ bc4py/chain/checking/__init__.py
@@ -1,4 +1,4 @@
-from bc4py.config import P
+from bc4py.config import P, BlockChainError
 from bc4py.chain.checking.checkblock import check_block, check_block_time
 from bc4py.chain.checking.checktx import check_tx, check_tx_time
 from bc4py.database.builder import builder, user_account
@@ -13,7 +13,7 @@ failed_deque = deque([], maxlen=10)
 
 def add_failed_mark():
     failed_deque.append(time.time())
-    if min(failed_deque) < time.time() - 7200:
+    if min(failed_deque) < time.time() - 3600:
         return
     elif len(failed_deque) >= 10:
         builder.make_failemark("Too many block check fail.")
@@ -24,11 +24,11 @@ def add_failed_mark():
 def new_insert_block(block, time_check=False):
     t = time.time()
     with global_lock:
-        fix_delay = time.time() - t
+        fixed_delay = time.time() - t
         try:
             # Check
             if time_check:
-                check_block_time(block, fix_delay)
+                check_block_time(block, fixed_delay)
             check_block(block)
             for tx in block.txs:
                 check_tx(tx=tx, include_block=block)
@@ -44,6 +44,13 @@ def new_insert_block(block, time_check=False):
                 P.NEW_CHAIN_INFO_QUE.put_nowait(('block', block.getinfo()))
             logging.info("New block accepted {}Sec {}.".format(round(time.time()-t, 3), block))
             return True
+        except BlockChainError as e:
+            logging.warning("Reject new block by \"{}\"".format(e))
+            delay = time.time() - builder.best_block.time
+            if delay > 10800:  # 3hours
+                logging.warning("{}Min before block inserted, too old on DB!".format(delay//60))
+                P.F_NOW_BOOTING = True
+            return False
         except BaseException as e:
             message = "New insert block error, \"{}\"".format(e)
             logging.warning(message, exc_info=True)
