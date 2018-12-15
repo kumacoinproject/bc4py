459daa1fcc921a0e4e828d84a11fe412638b3c0f
namuyan <thhjuu@yahoo.co.jp>
2018-12-15 20:11:42 +0900

    add paillier for so-called SecretContract

--- bc4py/contract/basiclib/__init__.py
+++ bc4py/contract/basiclib/__init__.py
@@ -1,5 +1,6 @@
 from bc4py.contract.basiclib.basic import *
-from bc4py.contract.basiclib import basic
+from bc4py.contract.basiclib.paillier import *
+from bc4py.contract.basiclib import basic, paillier
 from bc4py.user import Balance, Accounting
 import binascii
 
@@ -7,8 +8,9 @@ __all__ = [
     "Balance", "Accounting", "binascii",
 ]
 __all__ += basic.__all__
+__all__ += paillier.__all__
 
 
 __price__ = dict()
 __price__.update(basic.__price__)
-
+__price__.update(paillier.__price__)
