f43bc9ed99ad7d2e9f564da87ec0dda17b166dce
namuyan <thhjuu@yahoo.co.jp>
2019-01-11 19:19:11 +0900

    add: time_limit option to update_unspents_txs()

--- bc4py/user/generate.py
+++ bc4py/user/generate.py
@@ -257,16 +257,21 @@ def update_unconfirmed_txs(new_unconfirmed_txs):
     unconfirmed_txs = new_unconfirmed_txs
 
 
-def update_unspents_txs():
+def update_unspents_txs(time_limit=0.2):
     global unspents_txs
-    c = 50
-    while previous_block is None and 0 < c:
-        sleep(0.2)
+    c = 100
+    while previous_block is None:
+        if c < 0:
+            raise Exception('Timeout on update unspents.')
+        sleep(0.1)
         c -= 1
+    s = time()
     previous_height = previous_block.height
     proof_txs = list()
     all_num = 0
     for address, height, txhash, txindex, coin_id, amount in get_unspents_iter():
+        if time() - s > time_limit:
+            break
         if height is None:
             continue
         if coin_id != 0:
