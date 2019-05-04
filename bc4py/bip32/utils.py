b60de807d0dd9655a12493ab1ea5d769c63eff44
namuyan <thhjuu@yahoo.co.jp>
2019-05-04 21:12:03 +0900

    change: implement bech32 address fnc in Rust

--- bc4py/bip32/utils.py
+++ bc4py/bip32/utils.py
@@ -1,14 +1,14 @@
-from bc4py.bip32.bech32 import decode_with_check, encode_with_check
+from bc4py_extension import bech2address, address2bech
 import hashlib
 
 
 def is_address(ck, hrp, ver):
     """check bech32 format and version"""
     try:
-        addr_ver, addr_identifier = decode_with_check(hrp, ck)
-        if addr_ver is None:
+        n_hrp, n_ver, n_id = address2bech(ck)
+        if n_hrp != hrp:
             return False
-        if ver != addr_ver:
+        if n_ver != ver:
             return False
     except ValueError:
         return False
@@ -18,35 +18,29 @@ def is_address(ck, hrp, ver):
 def get_address(pk, hrp, ver):
     """get address from public key"""
     identifier = hashlib.new('ripemd160', hashlib.sha256(pk).digest()).digest()
-    return encode_with_check(hrp, ver, identifier)
+    return bech2address(hrp, ver, identifier)
 
 
 def convert_address(ck, hrp, ver):
     """convert address's version"""
-    _, identifier = decode_with_check(hrp, ck)
-    if identifier is None:
-        raise ValueError('Not correct format address hrp={} ck={}'.format(hrp, ck))
-    return encode_with_check(hrp, ver, identifier)
+    n_hrp, n_ver, n_id = address2bech(ck)
+    return bech2address(hrp, ver, n_id)
 
 
 def dummy_address(dummy_identifier):
     assert len(dummy_identifier) == 20
-    return encode_with_check('dummy', 0, dummy_identifier)
+    return bech2address('dummy', 0, dummy_identifier)
 
 
 def addr2bin(ck, hrp):
-    if ck.startswith('dummy'):
-        ver, identifier = decode_with_check('dummy', ck)
-    else:
-        ver, identifier = decode_with_check(hrp, ck)
-    if ver is None:
-        raise ValueError('Not correct format')
-    return ver.to_bytes(1, 'big') + identifier
+    n_hrp, n_ver, n_id = address2bech(ck)
+    assert n_hrp == 'dummy' or n_hrp == hrp
+    return n_ver.to_bytes(1, 'big') + n_id
 
 
 def bin2addr(b, hrp):
     ver, identifier = b[0], b[1:]
-    return encode_with_check(hrp, ver, identifier)
+    return bech2address(hrp, ver, identifier)
 
 
 __all__ = [
