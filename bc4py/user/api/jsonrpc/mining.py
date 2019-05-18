2990ea22b96e29e539c0d76617f2c85e5982b4b6
namuyan <thhjuu@yahoo.co.jp>
2019-05-18 20:24:00 +0900

    add: json-rpc commands do not forget subtract_fee_from_amount

--- bc4py/user/api/jsonrpc/mining.py
+++ bc4py/user/api/jsonrpc/mining.py
@@ -1,92 +1,28 @@
 from bc4py import __chain_version__
-from bc4py.config import C, V, P
-from bc4py.user.api import web_base
+from bc4py.config import C, V
 from bc4py.database.builder import builder, tx_builder
 from bc4py.user.generate import create_mining_block, confirmed_generating_block
 from bc4py.chain.block import Block
 from bc4py.chain.tx import TX
 from binascii import a2b_hex
-from aiohttp import web
-from base64 import b64decode
-import json
 from time import time
 from expiringdict import ExpiringDict
 import asyncio
-import traceback
 from logging import getLogger
 
+
 log = getLogger('bc4py')
-
-# about "coinbasetxn"
-# https://bitcoin.stackexchange.com/questions/13438/difference-between-coinbaseaux-flags-vs-coinbasetxn-data
-# https://github.com/bitcoin/bips/blob/master/bip-0022.mediawiki
-
 
 getwork_cashe = ExpiringDict(max_len=100, max_age_seconds=300)
 extra_target = None  # 0x00000000ffff0000000000000000000000000000000000000000000000000000
 
 
-async def json_rpc(request):
-    # JSON-RPC require BasicAuth
-    if 'Authorization' not in request.headers:
-        return res_failed("Not found Authorization", None)
-    authorization = request.headers['Authorization']
-    auth_type, auth_data = authorization.split()
-    if auth_type != 'Basic':
-        return res_failed("Not Basic Authorization", None)
-
-    # user     => no meaning
-    # password => mining consensus number by confing.py
-    user, password = b64decode(auth_data.encode()).decode().split(':')
-    post = await web_base.content_type_json_check(request)
-    if P.F_NOW_BOOTING:
-        return res_failed("Busy status", post.get('id'))
-
-    try:
-        # post format => {"id": id, "method": method, "params": [params]}
-        method = post['method']
-        params = post.get('params', list())  # sgminer don't have
-        log.debug("RpcRequest: method={} params={}".format(method, params))
-        if not isinstance(params, list):
-            return res_failed("Params is list. not {}".format(type(params)), post.get('id'))
-
-        # find method function and throw task
-        fnc = globals().get(method)
-        if fnc is None:
-            return res_failed("not found method {}".format(method), post.get('id'))
-        result = await fnc(*params, user=user, password=password)
-        log.debug("RpcResponse: {}".format(result))
-        return res_success(result, post.get('id'))
-    except Exception as e:
-        tb = traceback.format_exc()
-        log.debug("JsonRpcError: {}".format(e))
-        return res_failed(str(tb), post.get('id'))
-
-
-def res_failed(error, uuid):
-    return web.Response(
-        text=json.dumps({
-            'id': uuid,
-            'result': None,
-            'error': error
-        }), content_type='application/json')
-
-
-def res_success(result, uuid):
-    return web.Response(
-        text=json.dumps({
-            'id': uuid,
-            'result': result,
-            'error': None
-        }), content_type='application/json')
-
-
-async def get_mining_block(**kwargs):
+async def get_mining_block(consensus):
     """create raw mining block"""
     s = time()
     while True:
         try:
-            return create_mining_block(consensus=int(kwargs['password']))
+            return create_mining_block(consensus=consensus)
         except Exception as e:
             if time() - s > 5:
                 raise TimeoutError("Mining block creation failed by '{}'".format(e))
@@ -95,8 +31,10 @@ async def get_mining_block(**kwargs):
 
 async def getwork(*args, **kwargs):
     """
-    duplicated method "getwork"
     https://en.bitcoin.it/wiki/Getwork
+    Result:
+        1. "data"       (hex string, required) block data
+        2. "target"     (hex string, required) little endian hash target
     """
     if len(args) == 0:
         now = int(time() - V.BLOCK_GENESIS_TIME)
@@ -107,7 +45,7 @@ async def getwork(*args, **kwargs):
                 mining_block = block
                 break
         else:
-            mining_block = await get_mining_block(**kwargs)
+            mining_block = await get_mining_block(int(kwargs['password']))
             getwork_cashe[mining_block.merkleroot] = mining_block
             mining_block.bits2target()
         # Pre-processed SHA-2 input chunks
@@ -146,20 +84,67 @@ async def getwork(*args, **kwargs):
 
 async def getblocktemplate(*args, **kwargs):
     """
-    default method "getblocktemplate"
     https://en.bitcoin.it/wiki/Getblocktemplate
     For full specification, see BIPs 22, 23, 9, and 145:
         https://github.com/bitcoin/bips/blob/master/bip-0022.mediawiki
         https://github.com/bitcoin/bips/blob/master/bip-0023.mediawiki
         https://github.com/bitcoin/bips/blob/master/bip-0009.mediawiki#getblocktemplate_changes
         https://github.com/bitcoin/bips/blob/master/bip-0145.mediawiki
+
+    Arguments:
+    1. template_request         (json object, optional) A json object in the following spec
+         {
+           "mode":"template"    (string, optional) This must be set to "template", "proposal" (see BIP 23), or omitted
+           "capabilities":[     (array, optional) A list of strings
+               "support"          (string) client side supported feature, 'longpoll', 'coinbasetxn', 'coinbasevalue', 'proposal', 'serverlist', 'workid'
+               ,...
+           ],
+           "rules":[            (array, optional) A list of strings
+               "support"          (string) client side supported softfork deployment
+               ,...
+           ]
+         }
+
+    Result:
+    {
+      "version" : n,                    (numeric) The preferred block version
+      "previousblockhash" : "xxxx",     (string) The hash of current highest block
+      "transactions" : [                (array) contents of non-coinbase transactions that should be included in the next block
+        {
+          "data" : "xxxx",             (string) transaction data encoded in hexadecimal (byte-for-byte)
+           "txid" : "xxxx",             (string) transaction id encoded in little-endian hexadecimal
+           "hash" : "xxxx",             (string) hash encoded in little-endian hexadecimal (including witness data)
+           "depends" : [                (array) array of numbers
+              n                          (numeric) transactions before this one (by 1-based index in 'transactions' list) that must be present in the final block if this one is
+              ,...
+           ],
+           "fee": n,                    (numeric) difference in value between transaction inputs and outputs (in satoshis); for coinbase transactions, this is a negative Number of the total collected block fees (ie, not including the block subsidy); if key is not present, fee is unknown and clients MUST NOT assume there isn't one
+           "sigops" : n,                (numeric) total SigOps cost, as counted for purposes of block limits; if key is not present, sigop cost is unknown and clients MUST NOT assume it is zero
+           "weight" : n,                (numeric) total transaction weight, as counted for purposes of block limits
+           "required" : true|false      (boolean) if provided and true, this transaction must be in the final block
+        }
+        ,...
+      ],
+      "target" : "xxxx",                (string) The hash target
+      "curtime" : ttt,                  (numeric) current timestamp in seconds since epoch (Jan 1 1970 GMT)
+      "mutable" : [                     (array of string) list of ways the block template may be changed
+         "value"                          (string) A way the block template may be changed, e.g. 'time', 'transactions', 'prevblock'
+         ,...
+      ],
+      "noncerange" : "00000000ffffffff",(string) A range of valid nonces
+      "sizelimit" : n,                  (numeric) limit of block size
+      "bits" : "xxxxxxxx",              (string) compressed target of next block
+      "height" : n                      (numeric) The height of the next block
+    }
+    See https://en.bitcoin.it/wiki/BIP_0022 for full specification.
     """
     # capabilities = {"capabilities": ["coinbasetxn", "workid", "coinbase/append"]}
-    mining_block = await get_mining_block(**kwargs)
+    mining_block = await get_mining_block(int(kwargs['password']))
     mining_block.bits2target()
     template = {
         "version": mining_block.version,
         "previousblockhash": bin2hex(mining_block.previous_hash),
+        "transactions": None,
         "coinbasetxn": {
             # sgminer say, FAILED to decipher work from 127.0.0.1
             "data": mining_block.txs[0].b.hex()
@@ -167,7 +152,6 @@ async def getblocktemplate(*args, **kwargs):
         "target": bin2hex(mining_block.target_hash),
         "mutable": ["time", "transactions", "prevblock"],
         "noncerange": "00000000ffffffff",
-        "sigoplimit": 20000,
         "sizelimit": C.SIZE_BLOCK_LIMIT,
         "curtime": mining_block.time,  # block time
         "bits": mining_block.bits.to_bytes(4, 'big').hex(),
@@ -180,7 +164,6 @@ async def getblocktemplate(*args, **kwargs):
             "hash": bin2hex(tx.hash),
             "depends": list(),
             "fee": 0,
-            "sigops": len(mining_block.txs) - 1
         })
     template['transactions'] = transactions
     return template
@@ -192,7 +175,7 @@ async def submitblock(*args, **kwargs):
     Attempts to submit new block to network
     """
     if len(args) == 0:
-        return 'no argument found'
+        raise ValueError('no argument found')
     block_hex_or_obj = args[0]
     if isinstance(block_hex_or_obj, str):
         block_bin = a2b_hex(block_hex_or_obj)
@@ -247,14 +230,12 @@ async def submitblock(*args, **kwargs):
         return 'not satisfied work'
 
 
-async def getmininginfo(*args, **kwargs):
-    return None  # dummy
-
-
 def bin2hex(b):
     return b[::-1].hex()
 
 
 __all__ = [
-    "json_rpc",
+    "getwork",
+    "getblocktemplate",
+    "submitblock",
 ]
---
+++ bc4py/user/api/jsonrpc/others.py
@@ -0,0 +1,53 @@
+from bc4py import __version__, __chain_version__, __message__
+from bc4py.config import V
+from bc4py.utils import GompertzCurve
+from bc4py.bip32 import is_address
+from bc4py.database.builder import builder, user_account
+from bc4py.chain.difficulty import get_bits_by_hash
+from logging import getLogger
+
+
+log = getLogger('bc4py')
+
+
+async def getinfo(*args, **kwargs):
+    """
+    method "getinfo"
+    """
+    consensus = int(kwargs['password'])
+    best_block = builder.best_block
+    # difficulty
+    bits, target = get_bits_by_hash(previous_hash=best_block.hash, consensus=consensus)
+    difficulty = (0xffffffffffffffff // target) / 100000000
+    # balance
+    users = user_account.get_balance(confirm=6)
+    return {
+        "version": __version__,
+        "protocolversion": __chain_version__,
+        "balance": round(users.get(0, 0) / pow(10, V.COIN_DIGIT), 8),
+        "blocks": best_block.height,
+        "moneysupply": GompertzCurve.calc_total_supply(best_block.height),
+        "connections": len(V.PC_OBJ.p2p.user),
+        "testnet": V.BECH32_HRP == 'test',
+        "difficulty": round(difficulty, 8),
+        "paytxfee": round(V.COIN_MINIMUM_PRICE / pow(10, V.COIN_DIGIT), 8),
+        "errors": __message__,
+    }
+
+
+async def validateaddress(*args, **kwargs):
+    """
+    method "validateaddress"
+    """
+    if len(args) == 0:
+        raise ValueError('no argument found')
+    address = args[0]
+    return {
+        "isvalid": is_address(address, V.BECH32_HRP, 0),
+        "address": address}
+
+
+__all__ = [
+    "getinfo",
+    "validateaddress",
+]
