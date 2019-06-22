e0bd2748afc322c29e497bcf80437230a4c2e08d
namuyang <thhjuu@yahoo.co.jp>
2019-06-22 13:33:02 +0900

    Update __init__.py

--- bc4py/__init__.py
+++ bc4py/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.27-alpha'
+__version__ = '0.0.27-test'
 __chain_version__ = 0
 __block_version__ = 1
 __message__ = 'This is alpha version - use at your own risk, do not use for merchant applications'
